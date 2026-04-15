a636daddb4139d78de704990090ddfceddad38fb
ANAPAULA05082008 <a.batista05@escola.pr.gov.br>
2026-04-15 16:14:49 -0300

    15/04/2026

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 const botoes=document.querySelectorAll(".botao");
 console.log(botoes);
 
-for(let I=0; I<= botoes.length; I++){
-
+for (let I=0; I<= botoes.length; I++){
     console.log(botoes{i});
+    botoes{i}.onclick=funcion() {
+        botoes(i).classlist.add("ativo");
+
+
+
+        
+
+    }
+ 
 
 }
